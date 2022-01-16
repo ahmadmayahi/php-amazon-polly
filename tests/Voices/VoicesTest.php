c8baf0353b661aab2eef044f8909efa5b36d98f9
Ahmad Mayahi <am@ordrestyring.dk>
2022-01-16 22:21:59 +0100

    Voice Tests

--- tests/Voices/VoicesTest.php
+++ tests/Voices/VoicesTest.php
@@ -10,6 +10,12 @@ use AhmadMayahi\Polly\Enums\Voices\Chinese;
 use AhmadMayahi\Polly\Enums\Voices\Danish;
 use AhmadMayahi\Polly\Enums\Voices\Dutch;
 use AhmadMayahi\Polly\Enums\Voices\German;
+use AhmadMayahi\Polly\Enums\Voices\Hindi;
+use AhmadMayahi\Polly\Enums\Voices\Icelandic;
+use AhmadMayahi\Polly\Enums\Voices\Italian;
+use AhmadMayahi\Polly\Enums\Voices\Japanese;
+use AhmadMayahi\Polly\Enums\Voices\Korean;
+use AhmadMayahi\Polly\Enums\Voices\Norwegian;
 use AhmadMayahi\Polly\Tests\AbstractTest;
 
 class VoicesTest extends AbstractTest
@@ -88,6 +94,127 @@ class VoicesTest extends AbstractTest
         $this->assertEquals(
             new VoiceDescription(name: German::Marlene, gender: Gender::Female, neural: false, standard: true),
             German::Marlene->describe()
+        );
+    }
+
+    /** @test */
+    public function it_should_describe_vicki_in_german_language(): void
+    {
+        $this->assertEquals(Language::German, German::Vicki->language());
+
+        $this->assertEquals(
+            new VoiceDescription(name: German::Vicki, gender: Gender::Female, neural: true, standard: true),
+            German::Vicki->describe()
+        );
+    }
+
+    /** @test */
+    public function it_should_describe_hans_in_german_language(): void
+    {
+        $this->assertEquals(Language::German, German::Hans->language());
+
+        $this->assertEquals(
+            new VoiceDescription(name: German::Hans, gender: Gender::Male, neural: false, standard: true),
+            German::Hans->describe()
+        );
+    }
+
+    /** @test */
+    public function it_should_describe_anditi_in_hindi_language(): void
+    {
+        $this->assertEquals(Language::Hindi, Hindi::Aditi->language());
+
+        $this->assertEquals(
+            new VoiceDescription(name: Hindi::Aditi, gender: Gender::Female, neural: false, standard: true, bilingual: true),
+            Hindi::Aditi->describe()
+        );
+    }
+
+    /** @test */
+    public function it_should_describe_dora_in_icelandic_language(): void
+    {
+        $this->assertEquals(Language::Icelandic, Icelandic::Dora->language());
+
+        $this->assertEquals(
+            new VoiceDescription(name: Icelandic::Dora, gender: Gender::Female, neural: false, standard: true),
+            Icelandic::Dora->describe()
+        );
+    }
+
+    /** @test */
+    public function it_should_describe_karl_in_icelandic_language(): void
+    {
+        $this->assertEquals(Language::Icelandic, Icelandic::Karl->language());
+
+        $this->assertEquals(
+            new VoiceDescription(name: Icelandic::Karl, gender: Gender::Male, neural: false, standard: true),
+            Icelandic::Karl->describe()
+        );
+    }
+
+    /** @test */
+    public function it_should_describe_carla_in_italian_language(): void
+    {
+        $this->assertEquals(Language::Italian, Italian::Carla->language());
+
+        $this->assertEquals(
+            new VoiceDescription(name: Italian::Carla, gender: Gender::Female, neural: false, standard: true),
+            Italian::Carla->describe()
+        );
+    }
+
+    /** @test */
+    public function it_should_describe_bianca_in_italian_language(): void
+    {
+        $this->assertEquals(Language::Italian, Italian::Bianca->language());
+
+        $this->assertEquals(
+            new VoiceDescription(name: Italian::Bianca, gender: Gender::Female, neural: true, standard: true),
+            Italian::Bianca->describe()
+        );
+    }
+
+    /** @test */
+    public function it_should_describe_mizuki_in_japanese_language(): void
+    {
+        $this->assertEquals(Language::Japanese, Japanese::Mizuki->language());
+
+        $this->assertEquals(
+            new VoiceDescription(name: Japanese::Mizuki, gender: Gender::Female, neural: false, standard: true),
+            Japanese::Mizuki->describe()
+        );
+    }
+
+    /** @test */
+    public function it_should_describe_takumi_in_japanese_language(): void
+    {
+        $this->assertEquals(Language::Japanese, Japanese::Takumi->language());
+
+        $this->assertEquals(
+            new VoiceDescription(name: Japanese::Takumi, gender: Gender::Male, neural: true, standard: true),
+            Japanese::Takumi->describe()
+        );
+    }
+
+    /** @test */
+    public function it_should_describe_seoyeon_in_korean_language(): void
+    {
+        $this->assertEquals(Language::Korean, Korean::Seoyeon->language());
+
+        $this->assertEquals(
+            new VoiceDescription(name: Korean::Seoyeon, gender: Gender::Female, neural: true, standard: false),
+            Korean::Seoyeon->describe()
+        );
+    }
+
+    /** @test */
+    public function it_should_describe_liv_in_norwegian_language(): void
+    {
+        $this->assertEquals(Language::Norwegian, Norwegian::Liv->language());
+
+        $this->assertEquals(
+            new VoiceDescription(name: Norwegian::Liv, gender: Gender::Female, neural: false, standard: true),
+            Norwegian::Liv->describe()
         );
     }
 }
